50f12d32c5729ad149d8540e815f3aa504d55328
Alex Wang <alex.wang@chromatiqa.org>
2014-04-30 00:51:34 -0700

    WIP add name-setting view

---
+++ GetThere/Classes/GTLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  GTLoginViewController.h
+//  GetThere
+//
+//  Created by Alex Yuh-Rern Wang on 4/29/14.
+//  Copyright (c) 2014 Chromatiqa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTLoginViewController : UIViewController
+
+@end
