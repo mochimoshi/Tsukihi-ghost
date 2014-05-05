4e898ab410a8ef094308ccde651bbd1b658cefb1
Angela Yeung <ayyyeung@dn525f0u.sunet>
2014-05-04 18:48:21 -0700

    image is now button

---
+++ GetThere/GTImageViewExpansion.h
@@ -0,0 +1,13 @@
+//
+//  GTImageViewExpansion.h
+//  GetThere
+//
+//  Created by Angela Yeung on 5/4/14.
+//  Copyright (c) 2014 Chromatiqa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTImageViewExpansion : UIViewController
+
+@end
