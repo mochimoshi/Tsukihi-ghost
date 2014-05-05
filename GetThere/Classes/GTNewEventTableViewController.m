9ce65fb59996f6478138ba2976ec9f9d5ba66bde
Alex Wang <alex.wang@chromatiqa.org>
2014-05-04 20:25:09 -0700

    Add in placeholder contact list

--- GetThere/Classes/GTNewEventTableViewController.m
+++ GetThere/Classes/GTNewEventTableViewController.m
@@ -7,12 +7,18 @@
 //
 
 #import "GTNewEventTableViewController.h"
+
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+
 #import <RETableViewManager/RETableViewManager.h>
+#import <RETableViewManager/RETableViewOptionsController.h>
 
 @interface GTNewEventTableViewController ()<RETableViewManagerDelegate>
 
 @property (strong, nonatomic) RETableViewManager *manager;
 @property (strong, nonatomic) RETableViewSection *eventInfoSection;
+@property (strong, nonatomic) RETableViewSection *eventInviteSection;
 @property (strong, nonatomic) RETableViewSection *eventButton;
 
 @property (strong, nonatomic) RETextItem *eventNameItem;
@@ -20,6 +26,7 @@
 @property (strong, nonatomic) REDateTimeItem *eventStartTimeItem;
 @property (strong, nonatomic) REDateTimeItem *eventEndTimeItem;
 @property (strong, nonatomic) RELongTextItem *eventNoteItem;
+@property (strong, nonatomic) REMultipleChoiceItem *eventInviteesItem;
 @end
 
 @implementation GTNewEventTableViewController
@@ -39,6 +46,7 @@
     
     self.manager = [[RETableViewManager alloc] initWithTableView:self.tableView delegate:self];
     self.eventInfoSection = [self buildEventSection];
+    self.eventInviteSection = [self buildInviteSection];
     self.eventButton = [self buildButton];
 }
 
@@ -59,7 +67,7 @@
         self.eventEndTimeItem.inlineDatePicker = YES;
     }
     
-    self.eventNoteItem = [RELongTextItem itemWithValue:nil placeholder:@"Remember to finish milestone 4"];
+    self.eventNoteItem = [RELongTextItem itemWithValue:nil placeholder:@"Put event notes here! e.g. Aim to finish milestone 4"];
     self.eventNoteItem.cellHeight = 88;
     
     
@@ -68,6 +76,45 @@
     [section addItem:self.eventStartTimeItem];
     [section addItem:self.eventEndTimeItem];
     [section addItem:self.eventNoteItem];
+    
+    return section;
+}
+
+- (RETableViewSection *)buildInviteSection
+{
+    __weak GTNewEventTableViewController *weakSelf = self;
+    
+    RETableViewSection *section = [RETableViewSection sectionWithHeaderTitle:@"Invitations"];
+    [self.manager addSection:section];
+    
+    self.eventInviteesItem = [REMultipleChoiceItem itemWithTitle:@"Invite..." value:nil selectionHandler:^(REMultipleChoiceItem *item) {
+        [item deselectRowAnimated:YES];
+        
+        // Generate sample options
+        //
+        NSMutableArray *options = [[NSMutableArray alloc] initWithArray:@[@"Charles Chen", @"Fran Guo", @"Kosaki Onodera", @"Jessica Liu", @"Pearle Lun", @"Victoria Wang", @"James Wu", @"Angela Yeung"]];
+        
+        // Present options controller
+        //
+        RETableViewOptionsController *optionsController = [[RETableViewOptionsController alloc] initWithItem:item options:options multipleChoice:YES completionHandler:^{
+            [item reloadRowWithAnimation:UITableViewRowAnimationNone];
+        }];
+        
+        // Adjust styles
+        //
+        optionsController.delegate = weakSelf;
+        optionsController.style = section.style;
+        if (weakSelf.tableView.backgroundView == nil) {
+            optionsController.tableView.backgroundColor = weakSelf.tableView.backgroundColor;
+            optionsController.tableView.backgroundView = nil;
+        }
+        
+        // Push the options controller
+        //
+        [weakSelf.navigationController pushViewController:optionsController animated:YES];
+    }];
+    
+    [section addItem:self.eventInviteesItem];
     
     return section;
 }
