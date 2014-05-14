11ebb88942538574cee9005a563ffddaee952178
jliu12 <jliu12@stanford.edu>
2014-05-13 22:29:08 -0700

    making photo annotations

---
+++ GetThere/GTPhotoMapAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  GTPhotoMapAnnotation.h
+//  GetThere
+//
+//  Created by Jessica Liu on 5/13/14.
+//  Copyright (c) 2014 Chromatiqa. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface GTPhotoMapAnnotation : MKAnnotationView
+
+@end
---
+++ GetThere/GTPhotoMapAnnotation.m
@@ -0,0 +1,31 @@
+//
+//  GTPhotoMapAnnotation.m
+//  GetThere
+//
+//  Created by Jessica Liu on 5/13/14.
+//  Copyright (c) 2014 Chromatiqa. All rights reserved.
+//
+
+#import "GTPhotoMapAnnotation.h"
+
+@implementation GTPhotoMapAnnotation
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
